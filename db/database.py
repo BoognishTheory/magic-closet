8ab4edb6076b62b8c323970ca9c1c82d8984de2b
BoognishTheory <kclaset@gmail.com>
2026-05-20 21:43:49 -0500

    fix: point DB to Railway persistent volume

--- db/database.py
+++ db/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db.models import Base
 
-DATABASE_URL = "sqlite:///magic_closet.db"
+DATABASE_URL = "sqlite:////data/magic_closet.db"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
